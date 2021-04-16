f62eb8eb85df51839bce63f05c3c0e8cbf8616d2
Jordan Leven <jordan@heysparkbox.com>
2021-04-16 10:54:54 -0400

    feat: Use token manager in authorization

--- internal/router/get_authorization_token.go
+++ internal/router/get_authorization_token.go
@@ -5,12 +5,15 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/jordanleven/slack-onboarder/internal/slackclient"
+	"github.com/jordanleven/slack-onboarder/internal/tokenmanager"
 )
 
 func getAuthorizationToken(c *gin.Context) {
 	code := c.Query("code")
 	authorization := slackclient.GetAuthorization(code)
+	token := tokenmanager.SetToken(authorization)
+
 	c.JSON(http.StatusOK, gin.H{
-		"authorization": authorization,
+		"authorization": token,
 	})
 }
